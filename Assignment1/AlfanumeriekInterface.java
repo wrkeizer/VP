9ee188a9edb9f9d9f2968704fb7207c14feecc1b
wrkeizer <wisse@wisses-macbook-air.fritz.box>
2013-09-07 18:02:49 +0200

    Redundancy weggehaald

--- Assignment1/AlfanumeriekInterface.java
+++ Assignment1/AlfanumeriekInterface.java
@@ -1,54 +1,10 @@
 /**
- * 
- * @author Floris Golbach &amp; Wisse Keizer
- * 
- * @elementen 
- * 	Eén character
- * @structuur	
- * 	0-Dimensionaal
- * @domein
- * 	(Hoofd)letters en cijfers
  *
- * @constructor
- * 	Alfanumeriek(char c);
- * 		<dl>
- * 		<dt><b>PRE:</b><dd> Het character is alfanumeriek.
- * 		<dt><b>POST:</b><dd> De inhoud van het nieuwe object is een alfanumeriek character.
- * 		</dl>
- * 
- *  Alfanumeriek(Alfanumeriek src);
- * 		<dl>
- * 		<dt><b>PRE:</b><dd> -
- * 		<dt><b>POST:</b><dd> De inhoud van het nieuwe object is een kopie van het src-object.
- * 		</dl>
- */
-public interface AlfanumeriekInterface {
-    /** Retourneert het character.
-     * @preconditie
-     *	    -
-     * @postconditie
-     *	    Het character is geretourneerd.
-     **/
-	char value();
-	
-	/** Retourneert of het character een letter of getal is.
-     * @preconditie
-     *	    -
-     * @postconditie
-     *	    true: het character is een letter.<br>
-     *	    false: het character is een getal.
-     **/
-    boolean isLetter ();
-	
-}
-=======
-/**
- * 
  * @author Floris Golbach & Wisse Keizer
- * 
- * @elementen 
- * 	Eén character
- * @structuur	
+ *
+ * @elementen
+ * 	Een character
+ * @structuur
  * 	0-Dimensionaal
  * @domein
  * 	(Hoofd)letters en cijfers
@@ -59,7 +15,7 @@ public interface AlfanumeriekInterface {
  * 		<dt><b>PRE:</b><dd> Het character is alfanumeriek.
  * 		<dt><b>POST:</b><dd> De inhoud van het nieuwe object is een alfanumeriek character.
  * 		</dl>
- * 
+ *
  *  Alfanumeriek(Alfanumeriek src);
  * 		<dl>
  * 		<dt><b>PRE:</b><dd> -
