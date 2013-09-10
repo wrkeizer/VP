655a2051c3ffbcb468ca17e6c05554552c47c6a6
wrkeizer <wisse@Wisses-MacBook-Air.local>
2013-09-10 16:05:45 +0200

    Identifier+

--- Assignment1/IdentifierInterface.java
+++ Assignment1/IdentifierInterface.java
@@ -2,35 +2,57 @@
  *
  * @author Floris Golbach & Wisse Keizer
  * @elementen
- *	alfanumerieke characters van het type Alfanumeriek
+ *  characters van het type char
  * @structuur
  *	lineair
  * @domein
  *	alle strings met minstens één character, bestaande uit alfanumerieke characters en beginnende met een letter
  * @constructor
- *	Identifier(Alfanumeriek[] characters);
+ *	Identifier();
  *	    <dl>
- *		<dt><b>PRE-conditie</b><dd>characters bevat minstens één element en het eerste element in characters is een aphabet-letter
- *		<dt><b>POST-conditie</b><dd>De inhoud van het nieuwe
- *		Indentifier-object is een string die precies de elementen van characters in dezelfde volgorde bevat.
+ *		<dt><b>PRE-conditie</b><dd>-
+ *		<dt><b>POST-conditie</b><dd>Het nieuwe
+ *		Indentifier-object bevat een StringBuffer met een dummy character.
  *	    </dl>
  *	<br>
  *	Identifier (Identifier src);
  *	    <dl>
  *		<dt><b>PRE-conditie</b><dd>-
- *		<dt><b>POST-conditie</b><dd>De inhoud van het nieuwe
- *		Identifier-object is een kopie van src.
+ *		<dt><b>POST-conditie</b><dd>Het nieuwe
+ *		Identifier-object bevat een kopie van de inhoud van src.
  *	    </dl>
  **/
+
 public interface IdentifierInterface {
     
-    
-    /** Initialiseert het Identifier object tot een lege identifier.
+    /** Initialiseert het Identifier object tot een identifier met één character.
      * @preconditie
-     *	    -
+     *	    c is een letter a-Z.
      * @postconditie
-     *	    De stack is leeg.
+     *	    De identifier is geinitaliseerd en bevat een StringBuffer met een kopie van character c.
      **/
-    void init ();
+    void init (char c);
+    
+    /** Voegt een character toe aan de identifier
+     * @preconditie
+     *	    c is een letter a-Z of een cijfer 0-9.
+     * @postconditie
+     *	    Aan de StringBuffer van het identifier-object is een kopie van character c toegevoegd.
+     **/
+    void addChar(char c);
+    
+    
+    /** Retourneert de naam van het identifier-object
+     * @preconditie
+     *	    De identifier is geïnitialiseerd.
+     * @postconditie
+     *	    Een kopie van de StringBuffer van het identifier-object is geretourneerd
+    **/
+    StringBuffer getName();
+    
+    //void setName(StringBuffer s);
+    //void addString(StringBuffer s);
+    //int getSize();
+    //void removeChar();
     
 }
