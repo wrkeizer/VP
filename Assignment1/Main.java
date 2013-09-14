861b68942ff50e17bdd5ffde8c639f2ed3e1c6c4
unknown <floris.golbach@nubisonline.nl>
2013-09-14 17:55:40 +0200

    Made a checklist with everything I could think of

--- Assignment1/Main.java
+++ Assignment1/Main.java
@@ -1,13 +1,84 @@
 package assignment1;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Main {
 
 	Main(){
 		
 	}
 	
-	private final void start(){
+	private char nextChar(Scanner in){
+		return in.next().charAt(0);
+	}
+	
+	private boolean nextCharIsDigit (Scanner in) {
+		return in.hasNext("[0-9]");
+	} 
+	
+	private boolean nextCharIsLetter (Scanner in) {
+		return in.hasNext("[a-zA-Z]");
+	}
+	
+	private boolean nextCharIs(Scanner in, char c) {
+		return in.hasNext(Pattern.quote(c+""));
+	}
+	
+	private boolean isCorrect(Verzameling v){
+		//setup Scanner
+		Scanner in = new Scanner(System.in);
+		in.useDelimiter("");
 		
+		
+		/*
+		Checklist:			
+		
+		Read until '}'
+			-Read first character
+			-Keep reading until space character: ' '
+			-Add those characters to Identifier while reading
+			-Add Identifier to v
+			-Do same thing with next 'word'
+		
+		Possible errors:
+			-'{' is missing; done
+			-First character is wrong
+			-Another character is wrong
+			-'}' is missing
+		
+		Make sure every error has an appropriate error message
+		
+		How does an EOF character work?
+			 
+		*/		
+		
+		
+		while(/*v != geldig*/){
+			v.init();
+			System.out.print("Geef verzameling: ");
+			
+			if(nextCharIs(in, '{')){
+				
+				
+				
+			}else System.out.println("Verzameling moet beginnen met een'{'");
+		}
+		
+		in.close();
+		return true;
+	}
+	
+	private boolean readyToGo(Verzameling v1, Verzameling v2){
+		return isCorrect(v1) && isCorrect(v2);
+	}
+	
+	private void start(){
+		Verzameling v1 = new Verzameling(),
+					v2 = new Verzameling();
+		while(readyToGo(v1, v2)){
+			//do operations
+		}
 	}
 	
 	public static void main(String[] args) {
