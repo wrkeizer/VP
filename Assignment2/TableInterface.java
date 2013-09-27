f66f94146e1d1151ea03581add481cf2ec2487a2
wrkeizer <wisse@Wisses-MacBook-Air.local>
2013-09-27 17:16:59 +0200

    mim

--- Assignment2/TableInterface.java
+++ Assignment2/TableInterface.java
@@ -1,5 +1,18 @@
 package assignment2;
 
+/**	@elementen : key-value pairs of type KeyValuePair
+ *	@structuur : lineair
+ *	@domein : 	The elements in the table are sorted monotonically increasing by their key attribute.
+ *				All rows of elements of type KeyValuePair are valid values for a table.
+ *       		For every non-empty table the reference current is pointing to an 
+ *				element in the table.
+ *	@constructor - Table ();
+ *	<dl>
+ *		<dt><b>PRE-conditie</b><dd>		-
+ *		<dt><b>POST-conditie</b><dd> 	The new Table-object is the empty table.
+ * </dl> 
+ **/
+
 public interface TableInterface {
 	
 }
