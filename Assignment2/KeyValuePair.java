9f24bfa68e0d0fc3a9a6259e9143ecdc9d78b7f3
unknown <floris.golbach@nubisonline.nl>
2013-10-02 18:38:44 +0200

    KeyPairValue

--- Assignment2/KeyValuePair.java
+++ Assignment2/KeyValuePair.java
@@ -2,21 +2,36 @@ package assignment2;
 
 public class KeyValuePair<K extends Data, V extends Clonable> implements Data {
 	
-	K k;
-	V v;
+	private K k;
+	private V v;
 	
 	//Identifier
 	//Set?
 	
 	KeyValuePair(K k, V v) {
-		
+		this.k = k;
+		this.v = v;
+	}
+	
+	public K getKey(){
+		return k;
+	}
+	
+	public V getValue(){
+		return v;
 	}
 	
 	public int compareTo (Object o) {
-		return -2;
+		KeyValuePair<K,V> kvp = (KeyValuePair<K,V>) o;
+		if(k.compareTo(kvp.getKey()) == -1){
+			return -1;
+		}else if(k.compareTo(kvp.getKey()) == 1){
+			return 1;
+		}
+		return 0;
 	}
 	
 	public KeyValuePair<K,V> clone(){ 
-		return this;
+		return new KeyValuePair<K,V>((K) k.clone(), (V) v.clone());		
 	}
 }
