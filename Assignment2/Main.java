0755ee00007d67172fc8bd6c77bceeb8356c732f
unknown <floris.golbach@nubisonline.nl>
2013-10-10 14:24:55 +0200

    updatetje

--- Assignment2/Main.java
+++ Assignment2/Main.java
@@ -52,11 +52,10 @@ public class Main {
 		return id;
 	}
 	
-	private void readIdentifiers(Set set, Scanner in) throws APException{
+	private void readIdentifiers(Set set, Scanner line) throws APException{
 		while(nextCharIs(in, '}') == false) { // read in identifiers
-			if (!nextCharIsLetter(in)) {
-				System.out.println("Identifier should start with a letter.");
-				in.nextLine();//Bethlehem insert
+			if (!nextCharIsDigit(line)) {
+				line.nextLine();
 				throw new APException("Foutmelding");
 			}
 			Identifier id = readIdentifier(in); // read in individual identifier
@@ -78,33 +77,84 @@ public class Main {
 		}
 	}
 	
-	private Set readSet(Scanner in) throws APException{
-		Set set = new Set();
-		removeWhitespace(in);
-		if(!nextCharIs(in, '{')){
-			if (nextCharIsNewLine(in)) {
-				in.nextLine();//Keizer insert
-				throw new APException("Foutmelding");
-			}
-			System.out.println("Set should start with a '{'.");
-			in.nextLine();//Bethlehem insert
+	private Set readSet(Scanner line) throws APException{
+		//This method can only be called if the next char is a '{'.
+		//Reads a single set from the program.
+		line.next(); //Read away '{'
+		removeWhitespace(line);
+		Set set = new Set();		
+		readNumbers(set, line);
+		
+		line.next(); // Lose the '}'
+		removeWhitespace(line);
+		if(!nextCharIsNewLine(line)) {
+			line.nextLine();
 			throw new APException("Foutmelding");
 		}
-		in.next(); // Get rid of '{'
-		removeWhitespace(in);
 		
-		readIdentifiers(set, in);
+		line.nextLine();
+		return set;		
+	}
+	
+	private Set readComplexFactor(Scanner line) throws APException{
+		//This method can only be called if the next char is a '('.
+		//Reads a single complex factor from the program.
+		line.next(); //Read away '('.
+		String complexFactor = "";
+		while(!nextCharIs(line, ')')){// <- Crashes in case of ((aap + noot) * mies) for example.
+			complexFactor += nextChar(line);
+		}
+		Scanner expression = new Scanner(complexFactor);
+		Set set = readExpression(expression);
+		line.next(); //Read away ')'.
 		
-		in.next(); // Lose the '}'
 		removeWhitespace(in);
 		if(!nextCharIsNewLine(in)) {
-			System.out.println("There are characters outside of the set.");
-			in.nextLine();//Bethlehem insert
+			line.nextLine();
 			throw new APException("Foutmelding");
 		}
 		
-		in.nextLine();//Bethlehem insert
-		return set;		
+		line.nextLine();
+		return set;
+	}
+	
+	private Set readFactor(Scanner line) throws APException{
+		//Reads a single factor from the program.
+		removeWhitespace(line);
+		Set set = new Set();
+		if(nextCharIsLetter(line)){
+			Identifier id = readIdentifier(line);
+			//set = set corresponding with id.
+			//If nonexistent, APException.
+		}else if(nextCharIs(line, '(')){
+			set = readComplexFactor(line);
+		}else if(nextCharIs(line, '{')){
+			set = readSet(line);
+		}else{
+			line.nextLine();
+			throw new APException("Foutmelding");
+		}
+		line.nextLine();
+		return set;
+	}
+	
+	private Set readTerm(Scanner line) throws APException{
+		//Reads one or more factor(s), separated by a '*'.
+		removeWhitespace(in);
+		Set set = readFactor(line);
+		while(line.hasNext()){
+			removeWhitespace(line);
+			if(nextCharIsNewLine(line)){
+				line.nextLine();
+			}else if(nextCharIs(line, '*')){
+				line.next(); //Read away '*'.
+				set.intersection(readTerm(line));
+			}else{
+				line.nextLine();
+				throw new APException("Foutmelding");
+			}
+		}
+		return set;
 	}
 	
 	private Set readExpression(Scanner line) throws APException{
@@ -127,7 +177,6 @@ public class Main {
 			}else{
 				line.nextLine();
 				throw new APException("Foutmelding");
-				return null; //<- Is this correct?
 			}
 		}
 		return set;
